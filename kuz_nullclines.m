48427782bac3a0c09abf84f038fbea4fa4e35f3b
Audrey Yang <audrey.butter0930@gmail.com>
2023-04-14 21:26:52 -0400

    graph labeling

---
+++ kuz_nullclines.m
@@ -0,0 +1,7 @@
+plot(0:1:5, 0)
+
+title('Nullclines of Kuznetsov et al. (1994) model')
+% legend('s0 = 0.05','s0 = 0.1','s0 = 0.15','s0 = 0.2', 's0 = 0.25')
+axis([0 5 0 500])
+xlabel('Effector Cells (x)')
+ylabel('Tumor Cells (y)')
